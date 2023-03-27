9e4be30fd2fbe9b8eadfa644c3c6b0a52463aa1f
rileydevdzn <111300135+rileydevdzn@users.noreply.github.com>
2023-03-27 17:19:47 -0600

    Add HTML, JS

---
+++ script.js
@@ -0,0 +1,120 @@
+import lottieWeb from 'https://cdn.skypack.dev/lottie-web';
+
+class AudioPlayer extends HTMLElement {
+    constructor() {
+        super();
+        const template = document.querySelector('template');
+        const templateContent = template.content;
+        const shadow = this.attachShadow({mode: 'open'});
+        shadow.appendChild(templateContent.cloneNode(true));
+    }
+
+    connectedCallback() {
+        everything(this);
+    }
+}
+
+const everything = function(element) {  
+  const shadow = element.shadowRoot;
+
+    const playBtnContainer = shadow.getElementById('play-icon');
+    const audioPlayerContainer = shadow.getElementById('audio-player-container');
+    const seekSlider = shadow.getElementById('seek-slider');
+    let state = 'play';  
+    const audio = shadow.querySelector('audio');
+  
+    audio.src = element.getAttribute('data-src');
+  
+    const durationContainer = shadow.getElementById('duration-time');
+    const currentTimeContainer = shadow.getElementById('current-time');
+    let raf = null;
+
+
+    const animation = lottieWeb.loadAnimation({
+        container: playBtnContainer,
+        path: 'https://maxst.icons8.com/vue-static/landings/animated-icons/icons/pause/pause.json',
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Play Button Animation",
+    });
+          
+    animation.goToAndStop(14, true);
+
+    const whilePlaying = () => {
+        seekSlider.value = Math.floor(audio.currentTime);
+        currentTimeContainer.textContent = calculateTime(seekSlider.value);
+        audioPlayerContainer.style.setProperty('--seek-before-width', `${seekSlider.value / seekSlider.max * 100}%`);
+        raf = requestAnimationFrame(whilePlaying);
+    }
+
+    const showRangeProgress = (rangeInput) => {
+        if(rangeInput === seekSlider) audioPlayerContainer.style.setProperty('--seek-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+        else audioPlayerContainer.style.setProperty('--volume-before-width', rangeInput.value / rangeInput.max * 100 + '%');
+    }
+
+    const calculateTime = (secs) => {
+        const minutes = Math.floor(secs / 60);
+        const seconds = Math.floor(secs % 60);
+        const returnedSeconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+        return `${minutes}:${returnedSeconds}`;
+    }
+        
+    const displayDuration = () => {
+        durationContainer.textContent = calculateTime(audio.duration);
+    }
+        
+    const setSliderMax = () => {
+        seekSlider.max = Math.floor(audio.duration);
+    }
+        
+    const displayBufferedAmount = () => {
+        const bufferedAmount = Math.floor(audio.buffered.end(audio.buffered.length - 1));
+        audioPlayerContainer.style.setProperty('--buffered-width', `${(bufferedAmount / seekSlider.max) * 100}%`);
+    }
+
+    if (audio.readyState > 0) {
+        displayDuration();
+        setSliderMax();
+        displayBufferedAmount();
+    } else {
+        audio.addEventListener('loadedmetadata', () => {
+            displayDuration();
+            setSliderMax();
+            displayBufferedAmount();
+        });
+    }
+
+    playBtnContainer.addEventListener('click', () => {
+        if(state === 'play') {
+            audio.play();
+            animation.playSegments([14, 27], true);
+            requestAnimationFrame(whilePlaying);
+            state = 'pause';
+        } else {
+            audio.pause();
+            animation.playSegments([0, 14], true);
+            cancelAnimationFrame(raf);
+            state = 'play';
+        }
+    });
+
+    audio.addEventListener('progress', displayBufferedAmount);
+
+    seekSlider.addEventListener('input', (e) => {
+        showRangeProgress(e.target);
+        currentTimeContainer.textContent = calculateTime(seekSlider.value);
+        if(!audio.paused) {
+            cancelAnimationFrame(raf);
+        }
+    });
+
+    seekSlider.addEventListener('change', () => {
+        audio.currentTime = seekSlider.value;
+        if(!audio.paused) {
+            requestAnimationFrame(whilePlaying);
+        }
+    });
+}
+
+customElements.define('audio-player', AudioPlayer);
